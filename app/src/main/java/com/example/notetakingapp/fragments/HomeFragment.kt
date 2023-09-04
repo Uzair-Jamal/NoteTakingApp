09a29e3605e81493172115bc29637dfef6d2eb87
My Pc <syeduzair99jamal@gmail.com>
2023-09-04 16:10:24 -0700

    till HomeFragment

--- app/src/main/java/com/example/notetakingapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notetakingapp/fragments/HomeFragment.kt
@@ -4,16 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.notetakingapp.MainActivity
 import com.example.notetakingapp.R
+import com.example.notetakingapp.adapter.NoteAdapter
+import com.example.notetakingapp.databinding.FragmentHomeBinding
+import com.example.notetakingapp.viewmodel.NoteViewModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextListener {
+
+    private var _binding: FragmentHomeBinding ?= null
+    private val binding get() = _binding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdapter : NoteAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).noteViewModel
+
+        setUpRecyclerView()
+        binding.fabAddNote.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_homeFragment_to_newNoteFragment
+            )
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        TODO("Not yet implemented")
+    }
 }
